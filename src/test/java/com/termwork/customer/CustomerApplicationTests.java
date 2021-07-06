539d5c6ffe4491b21e1502754555c0cbef939a1f
naskete <1776795871@qq.com>
2021-07-06 13:37:39 +0800

    :tada: Initialize

---
+++ src/test/java/com/termwork/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.termwork.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
