95acd6bc778b9690c262284e39e0553e6896b400
naskete <1776795871@qq.com>
2021-07-13 22:05:03 +0800

    add price of room

--- src/main/java/com/termwork/customer/Dao/Room.java
+++ src/main/java/com/termwork/customer/Dao/Room.java
@@ -1,9 +1,10 @@
 package com.termwork.customer.Dao;
 
 public class Room {
-    private int id;                 // id 客房号
+    private int roomId;             // roomId 客房号
     private int number;             // number 历史入住人数
     private int type;               // type 类型
+    private double price;           // price 价格 ？/天
     private boolean status = false; // status 是否入住，默认未入住
 
     public Room(){
@@ -16,12 +17,12 @@ public class Room {
         this.type = type;
     }
 
-    public int getId() {
-        return id;
+    public int getRoomId() {
+        return roomId;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
     }
 
     public int getNumber() {
@@ -36,11 +37,19 @@ public class Room {
         return type;
     }
 
-    public boolean getStatus() {
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isStatus() {
         return status;
     }
 
-    public void changeStatus() {
-        this.status = !this.status;
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 }
