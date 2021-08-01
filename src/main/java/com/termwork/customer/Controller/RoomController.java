617aad3851791050fb04d859ee7a1328fcaef973
naskete <1776795871@qq.com>
2021-08-01 18:06:52 +0800

    simple func

--- src/main/java/com/termwork/customer/Controller/RoomController.java
+++ src/main/java/com/termwork/customer/Controller/RoomController.java
@@ -1,67 +1,106 @@
 package com.termwork.customer.Controller;
 
-import com.alibaba.fastjson.JSON;
+import com.termwork.customer.DTO.ResultJson;
+import com.termwork.customer.Service.CustomerService;
+import com.termwork.customer.Service.ManagerService;
+import com.termwork.customer.Service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api")
 public class RoomController {
-    String tip;
+    // TODO 优化返回值
+    // TODO 异常处理
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ManagerService managerService;
+
     // 登录
     @PostMapping("/login")
     public Object login(@RequestParam("username") String username, @RequestParam("password") String password){
-        if(username.equals("admin")&&password.equals("123123")){
-            tip = "{\"status\":200,\"message\":\"登录成功\"}";
-        } else {
-            tip = "{\"status\":400,\"message\":\"登录失败\"}";
-        }
-        return JSON.parse(tip);
+        if(username.equals("admin")&&password.equals("123123"))
+            return new ResultJson(200, "登录成功");
+        return new ResultJson(400,"登录失败");
     }
-    // 登记入住
-    @PostMapping("/checkin")
-    public Object checkIn(@RequestParam("roomID") int roomID, @RequestParam("id") long id, @RequestParam("name") String name, @RequestParam("phone") String phone){
 
-        return "";
+    // TODO 登记入住
+    @PostMapping("/checkin")
+    public Object checkIn(@RequestParam("roomID") int roomID, @RequestParam("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone){
+        if(managerService.checkIn(roomID, id, name, phone))
+            return new ResultJson(200, "登记成功");
+        return new ResultJson(400, "登记失败");
     }
 
     // 客房名单
+    // 单人间
     @GetMapping("/single")
     public Object singleRoomList(){
-        return  null;
+        return  roomService.getSingleRoom();
     }
 
+    // 双人间
     @GetMapping("/twin")
     public Object twinRoomList(){
-        return null;
+        return roomService.getTwinRoom();
     }
 
-    @GetMapping("/info/room")
-    public Object roomInfo(){
-        return null;
+    // 房间信息
+    @PostMapping("/info/room")
+    public Object roomInfo(@RequestParam("roomID") int roomId){
+        return roomService.getRoomInfo(roomId);
     }
 
+    // TODO 替换房间信息中的0/1改为单双人间？
+
+    // 添加房间
     @PostMapping("/addRoom")
-    public Object addRoom(@RequestParam("roomID") int roomID, @RequestParam("type") int type){
-        return null;
+    public Object addRoom(@RequestParam("roomID") int roomID, @RequestParam("type") int type, @RequestParam("price") double price){
+        if(roomService.addRoom(roomID,type,price)){
+            return new ResultJson(200, "添加成功");
+        } else {
+            return new ResultJson(400, "添加失败");
+        }
     }
 
-    @GetMapping("/deleteRoom")
+    // 删除房间
+    @PostMapping("/deleteRoom")
     public Object deleteRoom(@RequestParam("roomID") int roomID){
-        return null;
+        if(roomService.deleteRoom(roomID))
+            return new ResultJson(200,"删除成功");
+        return new ResultJson(400,"删除失败");
     }
 
-    @GetMapping("/info/customer")
-    public String customerInfo(@RequestParam("id") String id){
-        return null;
+    // TODO 获取旅客信息
+    @PostMapping("/info/customer")
+    public Object customerInfo(@RequestParam("id") String id){
+        return customerService.getCustomerInfo(id);
     }
 
-    @PostMapping("/info/customer/delete")
-    public Object deleteRecord(){
-        return null;
+    // TODO 删除旅客
+    @PostMapping("/deleteCustomer")
+    public Object deleteRecord(@RequestParam("id") String id){
+        if(customerService.deleteCustomer(id))
+            return new ResultJson(200, "删除成功");
+        return new ResultJson(400, "删除失败");
     }
 
     @PostMapping("/price")
-    public Object price(){
-        return null;
+    public Object price(@RequestParam("roomID") int roomId){
+        double price = managerService.price(roomId);
+        return new ResultJson(200, "success", price);
+    }
+
+    @PostMapping("/checkOut")
+    public Object checkOut(){
+        managerService.checkOut();
+        return new ResultJson(200, "checkout");
     }
 }
