9354afd3b84c58bdc22388ab051a0378abfbf41d
naskete <1776795871@qq.com>
2021-08-08 23:10:34 +0800

    auto increment

--- src/main/java/com/termwork/customer/Dao/Record.java
+++ src/main/java/com/termwork/customer/Dao/Record.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
 public class Record {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;         // id 记录号
 
     private String cusID;      // cusID 用户id
