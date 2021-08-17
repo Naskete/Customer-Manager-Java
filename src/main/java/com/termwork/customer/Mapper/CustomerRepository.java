38cea49e5ae0ad0994aed2b040d758b5dc965ba4
naskete <1776795871@qq.com>
2021-08-17 22:48:30 +0800

    feat:customer register

--- src/main/java/com/termwork/customer/Mapper/CustomerRepository.java
+++ src/main/java/com/termwork/customer/Mapper/CustomerRepository.java
@@ -5,14 +5,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String> {
-    @Modifying
     @Query("SELECT user FROM Customer user WHERE user.id = ?1")
     Customer getCustomerById(String id);
 
     @Modifying
     @Query("DELETE FROM Customer WHERE id = ?1")
-    boolean deleteCustomerInfo(String id);
+    @Transactional
+    void deleteCustomerInfo(String id);
+
+    @Modifying
+    @Query(value = "INSERT INTO customer(id, count, name, phone) VALUES (?1, 1, ?2, ?3)", nativeQuery = true)
+    @Transactional
+    void customerRegister(String id, String name, String phone);
 }
