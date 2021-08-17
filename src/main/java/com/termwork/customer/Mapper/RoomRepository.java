a7b8d4ac68b46aafb97b4876e85d7be7f304516a
naskete <1776795871@qq.com>
2021-08-17 22:45:44 +0800

    fix:return value

--- src/main/java/com/termwork/customer/Mapper/RoomRepository.java
+++ src/main/java/com/termwork/customer/Mapper/RoomRepository.java
@@ -1,32 +1,43 @@
 package com.termwork.customer.Mapper;
 
 import com.termwork.customer.Dao.Room;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
     @Modifying
     @Query("SELECT s FROM Room s WHERE s.type = 0")
     List<Room> getSingleRoom();
+
 
     @Modifying
     @Query("SELECT s FROM  Room s WHERE s.type = 1")
     List<Room> getTwinRoom();
 
-    @Modifying
+
     @Query("SELECT s FROM Room s WHERE s.roomId = ?1")
     Room getRoomByRoomId(int roomId);
 
-    @Insert("INSERT INTO room(room_id, number, price, status, type) " +
-            "VALUES(#{roomId}, 0, #{price}, false, #{type})")
-    boolean addRoom(@Param("roomId") int roomId, @Param("type") int type, @Param("price") double price);
+    @Modifying
+    @Query(value = "INSERT INTO room(room_id, number, price, status, type) VALUES (?1, 0, ?3, false, ?2)", nativeQuery = true)
+    @Transactional
+    void addRoom(@Param("roomId") int roomId, @Param("type") int type, @Param("price") double price);
 
     @Modifying
     @Query("DELETE FROM Room WHERE roomId = ?1 ")
-    boolean deleteRoom(int roomId);
+    @Transactional
+    void deleteRoom(int roomId);
+
+    @Modifying
+    @Query("UPDATE Room SET status=?2 WHERE roomId=?1")
+    @Transactional
+    void updateStatus(int roomId, boolean status);
 }
