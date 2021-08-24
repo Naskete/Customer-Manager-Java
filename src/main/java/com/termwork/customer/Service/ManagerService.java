14c6ba588ebfa847c56108015af58e4864a5119e
naskete <1776795871@qq.com>
2021-08-24 19:01:21 +0800

    feat:price and checkout

--- src/main/java/com/termwork/customer/Service/ManagerService.java
+++ src/main/java/com/termwork/customer/Service/ManagerService.java
@@ -4,12 +4,11 @@ import com.termwork.customer.Dao.Room;
 import com.termwork.customer.Mapper.CustomerRepository;
 import com.termwork.customer.Mapper.ManagerRepository;
 import com.termwork.customer.Mapper.RoomRepository;
+import com.termwork.customer.Util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
-import java.sql.Savepoint;
-
+import java.util.Date;
 
 @Service
 public class ManagerService {
@@ -23,31 +22,34 @@ public class ManagerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public boolean checkIn(int roomId, String id, String name, String phone){
+
+    public boolean checkIn(int roomId, String id, String name, String phone, String end) {
+        Date endTime = Util.toDate(end);
         Room room = roomRepository.getRoomByRoomId(roomId);
         // 已经有人入住
         if(room.isStatus()) {
             return false;
         }
         try {
-            managerRepository.checkIn(roomId, id);
+            managerRepository.checkIn(roomId, id, endTime);
             customerRepository.customerRegister(id, name, phone);
             roomRepository.updateStatus(roomId, true);
         } catch (Exception e){
-            e.printStackTrace();
-            return false;
+            throw e;
         }
         return true;
     }
 
-    public double price(int roomId){
+    public double price(int roomId, String start, String end){
         Room room = roomRepository.getRoomByRoomId(roomId);
-        System.out.println(room);
-
-        return room.getPrice();
+        long day = Util.getDays(start, end);
+        return room.getPrice()*day;
     }
 
-    public Object checkOut(){
-        return null;
+    public boolean checkOut(int roomId, String id){
+        roomRepository.updateStatus(roomId, false);
+        roomRepository.increase(roomId);
+        customerRepository.increase(id);
+        return true;
     }
 }
